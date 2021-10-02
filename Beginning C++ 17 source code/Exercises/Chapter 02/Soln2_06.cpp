c385f2da1262445d81408f6df36dbc771b559ed8
James Petersen <m@jamespetersen.ca>
2021-10-02 16:18:05 -0300

    Alternate method for exercise 2-6
    An alternate method for finding the larger and smaller of two numbers, while only using concepts presented in the second chapter, is to compute the average, average difference, and the remainder of the division of the difference and 2 (this is because of loss of precision with integer division). Then, the larger number is the sum of all three, while the smaller number is the difference between the average and the average difference.

--- Beginning C++ 17 source code/Exercises/Chapter 02/Soln2_06.cpp
+++ Beginning C++ 17 source code/Exercises/Chapter 02/Soln2_06.cpp
@@ -17,6 +17,23 @@ int main()
   // and the smaller of the two integers
   const long larger {(a * (a / b) + b * (b / a)) / (a / b + b / a)};
   const long smaller {(b * (a / b) + a * (b / a)) / (a / b + b / a)};
+  
+  // Alternate method:
+  // Find the average, average difference, whether the difference
+  // even or odd (0 if even, 1 if odd). The larger number is the sum
+  // of the three. The smaller number is the difference between the first two.
+  // The <cmath> header would have to be included for this to work.
+  //
+  // const long average {(a + b) / 2};
+  // const long difference {std::abs(a - b)};
+  // const long average_difference {difference / 2};
+  // const long difference_is_odd {difference % 2};
+  // const long larger {average + average_difference + difference_is_odd};
+  // const long smaller {average - average_difference};
+  //
+  // This method should require less computation than the method described above,
+  // and only uses operators and functions presented in the second chapter.
+  
   std::cout << "The larger integer is " << larger << ".\n"
             << "The smaller integer is " << smaller << '.' << std::endl;
 }
